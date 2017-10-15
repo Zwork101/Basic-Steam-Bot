bf52d4d909c331e816faf852dc58aeceb31f3d16
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2017-10-15 10:24:51 +0200

    Words
    More words

--- botfile.js
+++ botfile.js
@@ -32,3 +32,89 @@ client.on('loggedOn', () => {
                               have it in your library.
                               The number can be found here: http://store.steampowered.com/app/440/ */
 });
+
+client.on('friendRelationship', (steamid, relationship) => {
+    if (relationship === 2) {
+        client.addFriend(steamid);
+        client.chatMessage(steamid, config.addMessage);
+    }
+});
+
+client.on('webSession', (sessionid, cookies) => {
+	manager.setCookies(cookies);
+
+	community.setCookies(cookies);
+	community.startConfirmationChecker(20000, config.identitySecret);
+});
+
+function acceptOffer(offer) {
+	offer.accept((err) => {
+		community.checkConfirmations();
+		console.log("we Accepted an offer")
+		if (err) console.log("There was an error accepting the offer");
+	});
+}
+
+function declineOffer(offer) {
+	offer.decline((err) => {
+		console.log("we Declined an offer")
+		if (err) console.log("There was an error declining the offer");
+	});
+}
+
+function processOffer(offer) {
+	if (offer.isGlitched() || offer.state === 11) {
+	console.log("Offer was unavailable, declining.");
+	declineOffer(offer);
+	} else if (offer.partner.getSteamID64() === config.ownerID) {
+		acceptOffer(offer);
+	} else {
+		var ourItems = offer.itemsToGive;
+		var theirItems = offer.itemsToReceive;
+		var ourValue = 0;
+		var theirValue = 0;
+		for (var i in ourItems) {
+			var item = ourItems[i].market_name;
+			if(Prices[item]) {
+				ourValue += Prices[item].sell
+			} else {
+				console.log("Invalid Value, declining.")
+				ourValue+= 99999;
+			}
+		}
+		for (var i in theirItems) {
+			var item = theirItems[i].market_name;
+			if(Prices[item]) {
+				theirValue += Prices[item].buy;
+			} else {
+			console.log("Their item value was different.")
+		}
+	}
+	console.log("Our value: "+ourValue);
+	console.log("Their value: "+theirValue);
+	
+    if (ourValue <= theirValue) {
+        acceptOffer(offer);
+    } else {
+        declineOffer(offer);
+    }
+    }
+}
+
+manager.on('newOffer', (offer) => {
+	processOffer(offer);
+
+});
+
+/* manager.on('newOffer', (offer) => {
+	if (offer.partner.getSteamID64() === config.ownerID) {
+		acceptOffer(offer);
+	} else {
+		declineOffer(offer);
+	}
+}); */
+
+// client.setOption("promptSteamGuardCode", false);
+
+// Crafting soon
+
